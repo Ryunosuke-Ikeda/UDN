d1501831591605cc14bbd42d0979ed4f48c9c7ee
Leon Kunishi <49759672+Kamenleon@users.noreply.github.com>
2021-04-20 22:51:28 +0900

    add style.css

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server, {origins:'localhost:* 127.0.0.1:8124'});
+var io = require('socket.io')(server, { origins: 'localhost:* 127.0.0.1:8124' });
 server.listen(8124);
- 
+
+const path = require('path');
+
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname +'/oekaki/index.html');
 });
- 
+
 io.sockets.on('connection', function (socket) {
- 
+
     // クライアントからメッセージ受信
     socket.on('clear send', function () {
- 
+
         // 自分以外の全員に送る
         socket.broadcast.emit('clear user');
     });
- 
+
     // クライアントからメッセージ受信
     socket.on('server send', function (msg) {
- 
+
         // 自分以外の全員に送る
         socket.broadcast.emit('send user', msg);
     });
- 
+
     // 切断
     socket.on('disconnect', function () {
         io.sockets.emit('user disconnected');
     });
 });
+
+app.use(express.static(path.join(__dirname, '/oekaki')));//chat_roomディレクトリを公開
+console.log("Server started at port: " + 8124);
