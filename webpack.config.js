d6b15cd0214b7b4d491568fc705138c9fa1789b6
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-04-22 22:21:23 +0900

    build Sass and bundle by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,53 @@
 // output.pathに絶対パスを指定する必要があるため、pathモジュールを読み込んでおく
-
 const path = require('path');
 
 module.exports = {
 
-// モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
+  // モードの設定、v4系以降はmodeを指定しないと、webpack実行時に警告が出る
+  mode: 'development',
 
-mode: 'development',
+  // エントリーポイントの設定
+  entry: './oekakiChat/static/js/index.js',
 
-// エントリーポイントの設定
+  // 出力の設定
+  output: {
 
-entry: './oekakiChat/static/js/index.js',
+    // 出力するファイル名
+    filename: 'bundle.js',
 
-// 出力の設定
+    // 出力先のパス（v2系以降は絶対パスを指定する必要がある）
+    path: path.join(__dirname, '/oekakiChat/static/js')
 
-output: {
+  },
 
-// 出力するファイル名
+  module: {
+    rules: [
+      // Sassファイルの読み込みとコンパイル
+      {
+        test: /\.scss/, // 対象となるファイルの拡張子
+        use: [
+          // linkタグに出力する機能
+          "style-loader",
+          // CSSをバンドルするための機能
+          {
+            loader: "css-loader",
+            options: {
+              // オプションでCSS内のurl()メソッドの取り込みを禁止する
+              url: false,
 
-filename: 'bundle.js',
-
-// 出力先のパス（v2系以降は絶対パスを指定する必要がある）
-
-path: path.join(__dirname, '/oekakiChat/static/js')
-
-}
-
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  // ES5(IE11等)向けの指定（webpack 5以上で必要）
+  target: ["web", "es5"],
 };
