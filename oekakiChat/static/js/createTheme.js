5d6da95d0430547ab7e00e921db5a14d077df74d
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-04-24 16:29:55 +0900

    pタグを動的に書き換える

--- oekakiChat/static/js/createTheme.js
+++ oekakiChat/static/js/createTheme.js
@@ -1,16 +1,11 @@
 document.getElementById('createTheme').onclick = function createTheme() {
 
-    var random = Math.floor(Math.random() * 11);
+  var random = Math.floor(Math.random() * 11);
 
   $.getJSON("../static/js/theme.json", function (data) {
-    //↓actDataの配列内を検索
     for (var i = 0; i < Object.keys(data.themeData).length; i++) {
-      //↓結果と一致するnameの各データを取得し、画面に出力する
-      if ( data.themeData[random]== data.themeData[i]) {
-        var parent = document.getElementById("createTheme");
-        var p = document.createElement("p");
-        p.innerHTML = data.themeData[random].shiritoriTheme;
-        parent.appendChild(p);
+      if (data.themeData[random] == data.themeData[i]) {
+        document.getElementById("generateTheme").textContent = data.themeData[random].shiritoriTheme;
       }
     }
   })
