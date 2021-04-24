55ccc1454b42145f1e6985620c0d45c2ff125293
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-04-24 13:25:30 +0900

    loading 実装

---
+++ oekakiChat/static/js/loading.js
@@ -0,0 +1,16 @@
+$(function () {
+  var h = $(window).height();
+
+  $('#wrap').css('display', 'none');
+  $('#loader-bg ,#loader').height(h).css('display', 'block');
+});
+
+$(function () {
+  setTimeout('stopload()', 3000);
+});
+
+function stopload() {
+  $('#wrap').css('display', 'block');
+  $('#loader-bg').delay(900).fadeOut(800);
+  $('#loader').delay(600).fadeOut(300);
+}
