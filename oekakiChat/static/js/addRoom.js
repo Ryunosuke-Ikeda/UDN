67673ea6a26b6e7278e2a96a96930b49f4010c72
Leon Kunishi <49759672+Kamenleon@users.noreply.github.com>
2021-04-25 03:45:19 +0900

    roomNumber追加

--- oekakiChat/static/js/addRoom.js
+++ oekakiChat/static/js/addRoom.js
@@ -1,8 +1,10 @@
 window.onload = function () {
+  roomNumber = 1;
   for (var i = 3; i > 0; i--){
     document.getElementById("rouka").insertAdjacentHTML('beforeend', '<div id="grade' + i + '" class="grade">');
     for (var j = 1; j < 6; j++){
-      document.getElementById("grade"+ i).insertAdjacentHTML('beforeend', '<div id="' + i+ '-'+ j + '" class="classroom"><span>' + i + '-' + j + '</span><span><span class="material-icons">face</span></span></div>');
+      document.getElementById("grade"+ i).insertAdjacentHTML('beforeend', '<div id="' + i+ '-'+ j + '" class="classroom"><span>' + i + '-' + j + '</span><span><span class="material-icons">face</span><span id=room'+roomNumber+'>0</span></span></div>');
+      roomNumber += 1;
     }
     document.getElementById("rouka").insertAdjacentHTML('beforeend', '</div>');
   }
