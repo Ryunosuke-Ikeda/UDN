af87f611f6e3ab038a4c3c40d051b3d615b0fc54
Leon Kunishi <49759672+Kamenleon@users.noreply.github.com>
2021-04-25 05:50:20 +0900

    Bundle希望

--- oekakiChat/static/js/addRoom.js
+++ oekakiChat/static/js/addRoom.js
@@ -3,8 +3,9 @@ window.onload = function () {
   for (var i = 3; i > 0; i--){
     document.getElementById("rouka").insertAdjacentHTML('beforeend', '<div id="grade' + i + '" class="grade">');
     for (var j = 1; j < 6; j++){
-      document.getElementById("grade"+ i).insertAdjacentHTML('beforeend', '<div id="' + i+ '-'+ j + '" class="classroom"><span>' + i + '-' + j + '</span><span><span class="material-icons">face</span><span id=room'+roomNumber+'>1</span></span></div>');
+      document.getElementById("grade"+ i).insertAdjacentHTML('beforeend', '<div id="' + i+ '-'+ j + '" class="classroom" href="" data-target="modal'+roomNumber+'"><span>' + i + '-' + j + '</span><span><span class="material-icons">face</span><span id=room'+roomNumber+'>1</span></span></div>');
       roomNumber += 1;
+      document.getElementById("grade"+i).insertAdjacentHTML('beforeend', '</div id="modal'+roomNumber+'" class="modal js-modal"><div class="modal__bg js-modal-close"></div><div class="modal__content"><p>1つ目モーダルウィンドウです。ここにモーダルウィンドウで表示したいコンテンツを入れます。モーダルウィンドウを閉じる場合は下の「閉じる」をクリックするか、背景の黒い部分をクリックしても閉じることができます。</p><a class="js-modal-close" href="">閉じる</a></div></div>');
     }
     document.getElementById("rouka").insertAdjacentHTML('beforeend', '</div>');
   }
@@ -12,7 +13,7 @@ window.onload = function () {
 
 $(function(){
   var winScrollTop;
-  $('.js-modal-open').each(function(){
+  $('.classroom').each(function(){
       $(this).on('click',function(){
           //スクロール位置を取得
           winScrollTop = $(window).scrollTop();
