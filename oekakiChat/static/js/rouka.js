52aba2db94d2d0c1ae0a18a34ffacae531bb6e09
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2021-04-24 17:47:22 +0900

    廊下機能実装

---
+++ oekakiChat/static/js/rouka.js
@@ -0,0 +1,5 @@
+document.getElementById('sendButton').onclick = function rouka() {
+  document.getElementById("rouka").style = "display: none;";
+  document.getElementById("blackCover").style = "";
+  document.getElementById("center").style = "";
+}
