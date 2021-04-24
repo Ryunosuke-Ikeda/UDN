bc62845650c93222efab0d3f2af7afa850cf7533
Leon Kunishi <49759672+Kamenleon@users.noreply.github.com>
2021-04-25 04:50:56 +0900

    モーダル実験

--- oekakiChat/static/js/addRoom.js
+++ oekakiChat/static/js/addRoom.js
@@ -9,3 +9,22 @@ window.onload = function () {
     document.getElementById("rouka").insertAdjacentHTML('beforeend', '</div>');
   }
 }
+
+$(function(){
+  var winScrollTop;
+  $('.js-modal-open').each(function(){
+      $(this).on('click',function(){
+          //スクロール位置を取得
+          winScrollTop = $(window).scrollTop();
+          var target = $(this).data('target');
+          var modal = document.getElementById(target);
+          $(modal).fadeIn();
+          return false;
+      });
+  });
+  $('.js-modal-close').on('click',function(){
+      $('.js-modal').fadeOut();
+      $('body,html').stop().animate({scrollTop:winScrollTop}, 100);
+      return false;
+  }); 
+});
